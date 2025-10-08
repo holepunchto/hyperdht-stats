c7fcabb7cd88888b654af6e3ff7ae47c3242da03
hdegroote <75906619+HDegroote@users.noreply.github.com>
2025-10-08 10:15:00 +0200

    Lint with prettier

--- index.js
+++ index.js
@@ -1,65 +1,65 @@
 class HyperDhtStats {
-  constructor (dht) {
+  constructor(dht) {
     this.dht = dht
   }
 
-  get isFirewalled () {
+  get isFirewalled() {
     return this.dht.firewalled
   }
 
-  get punches () {
+  get punches() {
     return this.dht.stats.punches
   }
 
-  get relaying () {
+  get relaying() {
     return this.dht.stats.relaying || { attempts: 0, successes: 0, aborts: 0 } // For compat with hyperdht < 6.25.0
   }
 
-  get requests () {
+  get requests() {
     return this.dht.stats.requests || { active: 0, total: 0 } // for compat with dht-rpc < 6.20.1
   }
 
-  get queries () {
+  get queries() {
     return this.dht.stats.queries
   }
 
-  get pingCmds () {
+  get pingCmds() {
     return this.dht.stats.commands.ping
   }
 
-  get pingNatCmds () {
+  get pingNatCmds() {
     return this.dht.stats.commands.pingNat
   }
 
-  get downHintCmds () {
+  get downHintCmds() {
     return this.dht.stats.commands.downHint
   }
 
-  get findNodeCmds () {
+  get findNodeCmds() {
     return this.dht.stats.commands.findNode
   }
 
-  get clientSocketBytesTransmitted () {
+  get clientSocketBytesTransmitted() {
     return this.dht.io.clientSocket?.bytesTransmitted || 0
   }
 
-  get clientSocketPacketsTransmitted () {
+  get clientSocketPacketsTransmitted() {
     return this.dht.io.clientSocket?.packetsTransmitted || 0
   }
 
-  get clientSocketBytesReceived () {
+  get clientSocketBytesReceived() {
     return this.dht.io.clientSocket?.bytesReceived || 0
   }
 
-  get clientSocketPacketsReceived () {
+  get clientSocketPacketsReceived() {
     return this.dht.io.clientSocket?.packetsReceived || 0
   }
 
-  get streams () {
+  get streams() {
     return this.dht.rawStreams.size
   }
 
-  get pendingWrites () {
+  get pendingWrites() {
     let res = 0
     for (const s of this.dht.rawStreams) {
       res += s._wreqs.length - s._wfree.length
@@ -68,49 +68,49 @@ class HyperDhtStats {
     return res
   }
 
-  get serverSocketBytesTransmitted () {
+  get serverSocketBytesTransmitted() {
     return this.dht.io.serverSocket?.bytesTransmitted || 0
   }
 
-  get serverSocketPacketsTransmitted () {
+  get serverSocketPacketsTransmitted() {
     return this.dht.io.serverSocket?.packetsTransmitted || 0
   }
 
-  get serverSocketBytesReceived () {
+  get serverSocketBytesReceived() {
     return this.dht.io.serverSocket?.bytesReceived || 0
   }
 
-  get serverSocketPacketsReceived () {
+  get serverSocketPacketsReceived() {
     return this.dht.io.serverSocket?.packetsReceived || 0
   }
 
-  get udxBytesTransmitted () {
+  get udxBytesTransmitted() {
     return this.dht.udx.bytesTransmitted
   }
 
-  get udxPacketsTransmitted () {
+  get udxPacketsTransmitted() {
     return this.dht.udx.packetsTransmitted
   }
 
-  get udxBytesReceived () {
+  get udxBytesReceived() {
     return this.dht.udx.bytesReceived
   }
 
-  get udxPacketsReceived () {
+  get udxPacketsReceived() {
     return this.dht.udx.packetsReceived
   }
 
-  get udxPacketsDropped () {
+  get udxPacketsDropped() {
     if (this.dht.udx.packetsDroppedByKernel == null) return null
     if (this.dht.udx.packetsDroppedByKernel >= 2 ** 64) return 0 // not yet initialised (2**64 value normally)
     return this.dht.udx.packetsDroppedByKernel
   }
 
-  get nrNodes () {
+  get nrNodes() {
     return this.dht.nodes.length
   }
 
-  getRemoteAddress () {
+  getRemoteAddress() {
     const address = this.dht.firewalled
       ? { host: this.dht.host, port: this.dht.port }
       : this.dht.remoteAddress()
@@ -118,7 +118,7 @@ class HyperDhtStats {
     return `${address.host}:${address.port}`
   }
 
-  getLocalAddress () {
+  getLocalAddress() {
     const address = this.dht.localAddress()
     if (!address) return null
     return `${address.host}:${address.port}`
@@ -127,7 +127,7 @@ class HyperDhtStats {
   // Linear I.F.O. nodes length (could be constant by
   // listening to dht-rpc's node-added and node-removed events
   // and managing the state here)
-  get nrUniqueNodeIPs () {
+  get nrUniqueNodeIPs() {
     const ips = new Set()
     for (const node of this.dht.nodes.toArray()) {
       ips.add(node.host)
@@ -135,13 +135,13 @@ class HyperDhtStats {
     return ips.size
   }
 
-  get nrRecords () {
+  get nrRecords() {
     // TODO: use a public API
     return this.dht._persistent?.records.size || null
   }
   // TODO: nrMutables, nrImmutables
 
-  toJson () {
+  toJson() {
     return {
       isFirewalled: this.isFirewalled,
       localAddress: this.getLocalAddress(),
@@ -175,7 +175,7 @@ class HyperDhtStats {
     }
   }
 
-  toString () {
+  toString() {
     // Note: we do not include our remote address in the toString
     // because then sharing these stats could leak the IP
     return `DHT Stats
@@ -221,300 +221,338 @@ UDX Stats
   - udx_packets_dropped_total: ${this.udxPacketsDropped}`
   }
 
-  registerPrometheusMetrics (promClient) {
+  registerPrometheusMetrics(promClient) {
     const self = this
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       name: 'dht_consistent_punches',
       help: 'Total number of consistent holepunches performed by the hyperdht instance',
-      collect () {
+      collect() {
         this.set(self.punches.consistent)
       }
     })
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       name: 'dht_random_punches',
       help: 'Total number of random holepunches performed by the hyperdht instance',
-      collect () {
+      collect() {
         this.set(self.punches.random)
       }
     })
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       name: 'dht_open_punches',
       help: 'Total number of open holepunches performed by the hyperdht instance',
-      collect () {
+      collect() {
         this.set(self.punches.open)
       }
     })
 
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       name: 'dht_relay_attempts',
       help: 'Total relay attempts (as of 2025-10-06 only considers servers, not client connections)',
-      collect () {
+      collect() {
         this.set(self.relaying.attempts)
       }
     })
 
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       name: 'dht_relay_successes',
       help: 'Total relay successes (as of 2025-10-06 only considers servers, not client connections)',
-      collect () {
+      collect() {
         this.set(self.relaying.successes)
       }
     })
 
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       name: 'dht_relay_aborts',
       help: 'Total relay aborts (as of 2025-10-06 only considers servers, not client connections)',
-      collect () {
+      collect() {
         this.set(self.relaying.aborts)
       }
     })
 
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       name: 'dht_active_queries',
       help: 'Number of currently active queries in the dht-rpc instance',
-      collect () {
+      collect() {
         this.set(self.queries.active)
       }
     })
 
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       name: 'dht_total_queries',
       help: 'Total number of queries in the dht-rpc instance',
-      collect () {
+      collect() {
         this.set(self.queries.total)
       }
     })
 
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       name: 'dht_active_requests',
       help: 'Total number of active requests in the dht-rpc instance (one query can result in multiple requests)',
-      collect () {
+      collect() {
         this.set(self.requests.active)
       }
     })
 
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       name: 'dht_total_requests',
       help: 'Total number of requests made by the dht-rpc instance (one query can result in multiple requests)',
-      collect () {
+      collect() {
         this.set(self.requests.total)
       }
     })
 
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       name: 'dht_ping_received',
       help: 'Total number of PING commands received',
-      collect () {
+      collect() {
         this.set(self.pingCmds.rx)
       }
     })
 
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       name: 'dht_ping_transmitted',
       help: 'Total number of PING commands transmitted',
-      collect () {
+      collect() {
         this.set(self.pingCmds.tx)
       }
     })
 
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       name: 'dht_ping_nat_received',
       help: 'Total number of PING_NAT commands received',
-      collect () {
+      collect() {
         this.set(self.pingNatCmds.rx)
       }
     })
 
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       name: 'dht_ping_nat_transmitted',
       help: 'Total number of PING_NAT commands transmitted',
-      collect () {
+      collect() {
         this.set(self.pingNatCmds.tx)
       }
     })
 
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       name: 'dht_find_node_received',
       help: 'Total number of FIND_NODE commands received',
-      collect () {
+      collect() {
         this.set(self.findNodeCmds.rx)
       }
     })
 
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       name: 'dht_find_node_transmitted',
       help: 'Total number of FIND_NODE commands transmitted',
-      collect () {
+      collect() {
         this.set(self.findNodeCmds.tx)
       }
     })
 
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       name: 'dht_down_hint_received',
       help: 'Total number of DOWN_HINT commands received',
-      collect () {
+      collect() {
         this.set(self.downHintCmds.rx)
       }
     })
 
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       name: 'dht_down_hint_transmitted',
       help: 'Total number of DOWN_HINT commands transmitted',
-      collect () {
+      collect() {
         this.set(self.downHintCmds.tx)
       }
     })
 
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       name: 'udx_total_bytes_transmitted',
       help: 'Total bytes transmitted overall (by the UDX instance of the DHT)',
-      collect () {
+      collect() {
         this.set(self.udxBytesTransmitted)
       }
     })
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       name: 'udx_total_packets_transmitted',
       help: 'Total packets transmitted overall (by the UDX instance of the DHT)',
-      collect () {
+      collect() {
         this.set(self.udxPacketsTransmitted)
       }
     })
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       name: 'udx_total_bytes_received',
       help: 'Total bytes received overall (by the UDX instance of the DHT)',
-      collect () {
+      collect() {
         this.set(self.udxBytesReceived)
       }
     })
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       name: 'udx_total_packets_received',
       help: 'Total packets received overall (by the UDX instance of the DHT)',
-      collect () {
+      collect() {
         this.set(self.udxPacketsReceived)
       }
     })
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       name: 'udx_packets_dropped_total',
       help: 'Total packets dropped (by the UDX instance of the DHT). Only defined on Linux.',
-      collect () {
+      collect() {
         if (self.udxPacketsDropped !== null) this.set(self.udxPacketsDropped)
       }
     })
 
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       name: 'dht_streams',
       help: 'Total streams managed by the DHT',
-      collect () {
+      collect() {
         this.set(self.streams)
       }
     })
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       name: 'dht_pending_writes',
       help: 'Total pending writes on the streams of the DHT',
-      collect () {
+      collect() {
         this.set(self.pendingWrites)
       }
     })
 
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       name: 'dht_client_socket_bytes_transmitted',
       help: 'Total bytes transmitted by the client socket of the DHT',
-      collect () {
+      collect() {
         this.set(self.clientSocketBytesTransmitted)
       }
     })
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       name: 'dht_client_socket_packets_transmitted',
       help: 'Total packets transmitted by the client socket of the DHT',
-      collect () {
+      collect() {
         this.set(self.clientSocketPacketsTransmitted)
       }
     })
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       name: 'dht_client_socket_bytes_received',
       help: 'Total bytes received by the client socket of the DHT',
-      collect () {
+      collect() {
         this.set(self.clientSocketBytesReceived)
       }
     })
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       name: 'dht_client_socket_packets_received',
       help: 'Total packets received by the client socket of the DHT',
-      collect () {
+      collect() {
         this.set(self.clientSocketPacketsReceived)
       }
     })
 
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       name: 'dht_server_socket_bytes_transmitted',
       help: 'Total bytes transmitted by the server socket of the DHT',
-      collect () {
+      collect() {
         this.set(self.serverSocketBytesTransmitted)
       }
     })
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       name: 'dht_server_socket_packets_transmitted',
       help: 'Total packets transmitted by the server socket of the DHT',
-      collect () {
+      collect() {
         this.set(self.serverSocketPacketsTransmitted)
       }
     })
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       name: 'dht_server_socket_bytes_received',
       help: 'Total bytes received by the server socket of the DHT',
-      collect () {
+      collect() {
         this.set(self.serverSocketBytesReceived)
       }
     })
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       name: 'dht_server_socket_packets_received',
       help: 'Total packets received by the server socket of the DHT',
-      collect () {
+      collect() {
         this.set(self.serverSocketPacketsReceived)
       }
     })
 
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       name: 'dht_nr_nodes',
       help: 'Total nodes in the Kademlia DHT table of this node',
-      collect () {
+      collect() {
         this.set(self.nrNodes)
       }
     })
 
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       name: 'dht_nr_unique_node_ips',
       help: 'Total unique ip addresses in the Kademlia DHT table of this node',
-      collect () {
+      collect() {
         this.set(self.nrUniqueNodeIPs)
       }
     })
 
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       name: 'dht_is_firewalled',
       help: 'Whether the DHT server thinks it is behind a firewall (1) or not (0)',
-      collect () {
+      collect() {
         this.set(self.isFirewalled ? 1 : 0)
       }
     })
 
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       // Gauges expect a number, so we set the address as label instead
       name: 'dht_remote_address',
       help: 'The remote address of the DHT (set only if not firewalled)',
       labelNames: ['address'],
-      collect () {
+      collect() {
         const address = self.getRemoteAddress()
         if (address) this.labels(address).set(1)
       }
     })
 
-    new promClient.Gauge({ // eslint-disable-line no-new
+    new promClient.Gauge({
+      // eslint-disable-line no-new
       // Gauges expect a number, so we set the address as label instead
       name: 'dht_local_address',
       help: 'The local address of the DHT (set only if not firewalled)',
       labelNames: ['address'],
-      collect () {
+      collect() {
         const address = self.getLocalAddress()
         if (address) this.labels(address).set(1)
       }
@@ -523,10 +561,11 @@ UDX Stats
     // Only if the DHT is persistent (returns 0 values if it was
     // persistent for a while but now no longer)
     self.dht.once('persistent', () => {
-      new promClient.Gauge({ // eslint-disable-line no-new
+      new promClient.Gauge({
+        // eslint-disable-line no-new
         name: 'dht_nr_records',
         help: 'Total nr of records stored by this (persistent) node',
-        collect () {
+        collect() {
           this.set(self.nrRecords || 0)
         }
       })
